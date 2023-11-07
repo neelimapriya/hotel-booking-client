8a38e23046b2b35e77b6531f10a88b8fd92902f7
neelimapriya <neelimasultana6@gmail.com>
2023-11-07 18:38:51 +0600

    setup booking delete operation

--- src/Booking/BookingCard.jsx
+++ src/Booking/BookingCard.jsx
@@ -1,11 +1,18 @@
 import { MdDelete } from 'react-icons/md';
 import { LuPencilLine } from 'react-icons/lu';
+import Swal from 'sweetalert2';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
 
-const BookingCard = ({ books }) => {
-  const { img1, price, size, title, checkin, checkout } = books;
+const BookingCard = ({ books,mutate }) => {
+    
+  const { img1, price, size, title, checkin, checkout, _id } = books;
+
+
+
   return (
     <div>
-      <div className="w-60 mt-10 bg-slate-100 rounded-lg">
+      <div className="w-60 mt-10 shadow-lg rounded-lg">
         <div className="">
           <h5 className="mt-3 text-2xl font-serif font-semibold text-orange-800 text-center mb-2">
             {title}
@@ -31,14 +38,14 @@ const BookingCard = ({ books }) => {
               </tbody>
             </table>
           </div>
-          <div className="flex justify-around">
+          <div className="flex justify-around text-sm">
             <p>Price: <span className="text-orange-800">${price}</span></p>
             <p>size: <span className="text-orange-800">{size}</span></p>
           </div>
         </div>
         <div className="p-2 pt-0 text-sm text-black ">
-         <div className='flex items-center justify-around gap-4'><button>Update booking date</button><LuPencilLine className='mt-2 text-2xl'></LuPencilLine></div>
-         <div className='flex items-center gap-4 justify-around'><button>Cancel your booking</button><MdDelete className='mt-2 text-red-600 text-2xl'></MdDelete></div>
+         <div className='flex items-center justify-around gap-4'><button>Update booking date</button><LuPencilLine className='mt-2 hover:cursor-pointer text-2xl'></LuPencilLine></div>
+         <div onClick={()=>mutate(_id)} className='flex items-center gap-4 justify-around'><button >Cancel your booking</button><MdDelete className='mt-2 hover:cursor-pointer text-red-600 text-2xl'></MdDelete></div>
         </div>
       </div>
     </div>

e2b3df693aae07982d2ae5a2627f925036b1c70f
neelimapriya <neelimasultana6@gmail.com>
2023-11-08 17:14:18 +0600

    make contact page functional

--- src/Home/About/Contact.jsx
+++ src/Home/About/Contact.jsx
@@ -1,11 +1,43 @@
 import { Helmet } from 'react-helmet';
 import { AiOutlineArrowRight } from 'react-icons/ai';
+import Swal from "sweetalert2";
+// contact
 const Contact = () => {
+
+  const handleSend = (e) => {
+    e.preventDefault();
+    const form= e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const text = form.text.value;
+    console.log(email,name,text);
+    const contactData={email, name, text}
+
+    fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(contactData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire("Thank you!", "We will contact you soon!", "success");
+          form.reset()
+        }
+      });
+  };
+
+
   return (
-    <div className="mt-20 p-5 flex flex-col md:flex-row max-w-5xl mx-auto text-black font-serif">
-       <Helmet>
+    <div>
+      <Helmet>
         <title>Lartisien | Contact</title>
       </Helmet>
+      <div className="mt-20 p-5 flex flex-col md:flex-row max-w-5xl mx-auto text-black font-serif">
+       
       <div className=" md:w-1/2">
         <img src="https://i.ibb.co/tMXk6D6/LogoL.png" alt="" className="w-16" />
         <h2 className="text-2xl font-semibold font-serif mb-3">Address</h2>
@@ -25,13 +57,14 @@ const Contact = () => {
       <div className="md:w-1/2 mt-10 md:mt-0 md:ml-20">
         <h2 className="text-2xl font-semibold font-serif">SEND US A MESSAGE</h2>
         <hr className="w-52 " />
-        <form className="">
+        <form onSubmit={handleSend} className="">
           <div className="form-control">
             <label className="label">
               <span className="label-text font-serif">Name</span>
             </label>
             <input
               type="text"
+              name="name"
               placeholder="First and Last Name"
               className="border border-black p-2"
               required
@@ -43,6 +76,7 @@ const Contact = () => {
             </label>
             <input
               type="email"
+              name="email"
               placeholder="email"
               className="border w-full border-black p-2"
               required
@@ -61,10 +95,11 @@ const Contact = () => {
             ></textarea>
           </div>
           <div className="form-control mt-6">
-            <button className="btn bg-black text-white hover:bg-slate-700">Send</button>
+            <input type='submit' value='Send' className="btn bg-black text-white hover:bg-slate-700"/>
           </div>
         </form>
       </div>
+    </div>
     </div>
   );
 };
